d0939b8e7ab073d9d2cf56697485b90b5d118b99
Timur_Hook <davletshin.talan@gmail.com>
2019-07-31 21:35:58 +0500

    3.4 Скринкаст по Angular – Компоненты и директивы – Проекция контента с помощью ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserCardComponent } from './header/user-card/user-card.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserCardComponent
+    UserCardComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
