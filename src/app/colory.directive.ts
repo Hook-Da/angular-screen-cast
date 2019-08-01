23a701454e3f420f00a7023af2c7789aabfef07b
Timur_Hook <davletshin.talan@gmail.com>
2019-08-01 11:55:05 +0500

    Экспорт директивы через ссылку #

--- src/app/colory.directive.ts
+++ src/app/colory.directive.ts
@@ -1,15 +1,24 @@
 import { Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
-  selector: '[appColory]'
+  selector: '[appColory]',
+  exportAs: 'colory'
 })
+/*
+*  У директивы есть свойство exportAs с помощью которого мы можем указать
+*  с каким именем мы хотим экспортировать наш класс
+*/
 export class ColoryDirective {
 	@HostBinding('style.color') myColor: string;
 
 	@HostListener('click',['$events'])changeColor(event){
-		this.myColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+		this.setRandomColor();
 	}
 	
+  setRandomColor(){
+     this.myColor = '#'+Math.floor(Math.random()*16777215).toString(16); 
+  }
+
   constructor() { 
   	this.myColor = 'red';
   	setTimeout(_=>{
